50f813a8be41a92965af323d195048075d7c49c1
zx0286 <1061471799@qq.com>
2024-03-11 18:00:01 +0800

    1

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,5 @@ export const CMS_NAME = "Markdown";
 export const HOME_OG_IMAGE_URL =
   "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
 
-export const MS_NAME = "Jun Xi";
-export const MR_NAME = "Da Tou";
+export const MS_NAME = "TIGER";
+export const MR_NAME = "OX";
