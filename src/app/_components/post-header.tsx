7b88e21a23bff5a46f301b9ea3dabda790f0e021
liuy <1061471799@qq.com>
2024-03-14 00:00:49 +0800

    1

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -24,8 +24,7 @@ export function PostHeader({ title, coverImage, date, author }: Props) {
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
           <Avatar name={author.name} picture={author.picture} />
-        </div>
-        <div className="mb-6 text-lg">
+          {'Create at '}
           <DateFormatter dateString={date} />
         </div>
       </div>
