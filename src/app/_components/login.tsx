37b813de358a0206c117d8712c28bd25b2a8a157
zx0286 <1061471799@qq.com>
2024-03-15 10:13:03 +0800

    2222

--- src/app/_components/login.tsx
+++ src/app/_components/login.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {  useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 
 const Login = ({ originURL }: any) => {
@@ -21,7 +21,7 @@ const Login = ({ originURL }: any) => {
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" action="#" method="POST" id="login">
-          <div>
+          {/* <div>
             <label
               htmlFor="email"
               className="block text-sm font-medium leading-6 text-gray-900"
@@ -38,7 +38,7 @@ const Login = ({ originURL }: any) => {
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
-          </div>
+          </div> */}
 
           <div>
             <div className="flex items-center justify-between">
