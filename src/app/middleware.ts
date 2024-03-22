8256374fdf4f925c9121bbc607cf51a1e77581ce
zx0286 <1061471799@qq.com>
2024-03-22 10:39:52 +0800

    2

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,8 +4,10 @@ import { cookies } from "next/headers";
 export function middleware(request: Request) {
   const cookieStore = cookies();
   const user = cookieStore.get("auth");
-  if (!user) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  if (user) {
+    return NextResponse.redirect(
+      new URL(`/login?info${JSON.stringify(user)}`, request.url)
+    );
   }
 }
 
