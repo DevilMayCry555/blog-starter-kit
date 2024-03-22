ced9a6c83d360c4847b09d9741d627e127a35f4e
zx0286 <1061471799@qq.com>
2024-03-22 14:24:50 +0800

    2333

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -20,6 +20,7 @@ export function getPostBySlug(slug: string) {
 
 export function getAllPosts(): Post[] {
   const slugs = getPostSlugs();
+  console.log("slugs", slugs);
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
     // sort posts by date in descending order
