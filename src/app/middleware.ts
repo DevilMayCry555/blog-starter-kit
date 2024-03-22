2f6a72b8992ba953520fb920924aa4feaff62eaf
zx0286 <1061471799@qq.com>
2024-03-22 10:36:05 +0800

    1

---
+++ src/app/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export function middleware(request: Request) {
+  const cookieStore = cookies();
+  const user = cookieStore.get("auth");
+  if (!user) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: "*",
+};
