98470541ffb8b519b8e7b25940d645e88bcd255e
zx0286 <1061471799@qq.com>
2024-03-22 11:14:04 +0800

    6

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,17 @@
+export async function GET(request: Request) {
+  return {
+    key: "user",
+  };
+}
+
+export async function HEAD(request: Request) {}
+
+export async function POST(request: Request) {}
+
+export async function PUT(request: Request) {}
+
+export async function DELETE(request: Request) {}
+
+export async function PATCH(request: Request) {}
+
+export async function OPTIONS(request: Request) {}
