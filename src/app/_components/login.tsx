c23a23c68b0630c33a0164aa9e888987138b2a33
zx0286 <1061471799@qq.com>
2024-03-28 12:08:17 +0800

    1

--- src/app/_components/login.tsx
+++ src/app/_components/login.tsx
@@ -79,7 +79,7 @@ const Login = ({ originURL }: any) => {
             >
               <button
                 // type="submit"
-                disabled={!!Pwd}
+                disabled={!Pwd}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
