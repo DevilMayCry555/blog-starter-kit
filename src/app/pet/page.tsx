9416fe11594d6bfee71fbc1e6063842a25a468d9
zx0286 <1061471799@qq.com>
2024-04-09 18:02:49 +0800

    1

--- src/app/pet/page.tsx
+++ src/app/pet/page.tsx
@@ -6,9 +6,9 @@ import { DrawingBoard } from "../../lib/canvas";
 export default function Pet() {
   useEffect(() => {
     const ctx = new DrawingBoard("pet").context;
-    ctx.moveTo(50, 50);
-    ctx.lineTo(50, 100);
-    ctx.stroke();
+    // ctx.moveTo(50, 50);
+    // ctx.lineTo(50, 100);
+    // ctx.stroke();
   }, []);
   return (
     <div className="py-4">
