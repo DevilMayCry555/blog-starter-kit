4e88ed11337ecf6877e5c0a3742f4b6f9b39899c
zx0286 <1061471799@qq.com>
2024-05-10 15:20:09 +0800

    1

--- src/app/api/open/route.js
+++ src/app/api/open/route.js
@@ -6,8 +6,9 @@ export async function GET(request) {
   const { search } = request.nextUrl;
   const { route, ...rest } = qs(search);
   if (route === "task") {
-    const { type } = rest;
-    const { rows } = await sql`SELECT * FROM tasks WHERE type = ${type};`;
+    const { type, identity } = rest;
+    const { rows } =
+      await sql`SELECT * FROM tasks WHERE user_id = ${identity} AND type = ${type};`;
     return NextResponse.json({ rows }, { status: 200 });
   }
   return NextResponse.json({ error: "404" }, { status: 500 });
