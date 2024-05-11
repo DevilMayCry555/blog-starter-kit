6eb832b3270d43b7d5f350e048c8b0ef8af893a8
zx0286 <1061471799@qq.com>
2024-05-11 10:51:23 +0800

    1

--- src/app/api/open/route.js
+++ src/app/api/open/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { qs } from "@/lib/utils";
 import { sql } from "@vercel/postgres";
+import { format } from "date-fns";
 
 export async function GET(request) {
   const { search } = request.nextUrl;
@@ -12,4 +13,20 @@ export async function GET(request) {
     return NextResponse.json({ rows }, { status: 200 });
   }
   return NextResponse.json({ error: "404" }, { status: 500 });
+}
+export async function POST(request) {
+  const { value } = await request.body.getReader().read();
+  const { identity, title, content, type, points } = JSON.parse(
+    decoder.decode(value)
+  );
+  const time = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+  const uid = Date.now();
+  await sql`INSERT INTO tasks (uid,user_id,title,content,type,points,create_time)
+  VALUES (${uid},${identity},${title},${content},${type},${points},${time});`;
+  return NextResponse.json(
+    {
+      data: true,
+    },
+    { status: 200 }
+  );
 }
