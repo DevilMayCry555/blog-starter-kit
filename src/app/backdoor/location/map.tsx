8c1f3008aa3faf78985291ec9ce668d031756a04
liuy <1061471799@qq.com>
2024-05-21 02:31:30 +0800

    1

--- src/app/backdoor/location/map.tsx
+++ src/app/backdoor/location/map.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./map.css";
 
 interface Prop {
@@ -8,7 +8,24 @@ interface Prop {
 }
 export default function AMapContainer({ locations }: Prop) {
   let map: any = null;
+  const [la, set_la] = useState(0);
+  const [lo, set_lo] = useState(0);
 
+  useEffect(() => {
+    const [la, lo] = location.hash.split("/").slice(1);
+    set_la(+la);
+    set_lo(+lo);
+    window.addEventListener(
+      "hashchange",
+      () => {
+        // console.log("The hash has changed!", location.hash.split("/").slice(1));
+        const [la, lo] = location.hash.split("/").slice(1);
+        set_la(+la);
+        set_lo(+lo);
+      },
+      false
+    );
+  }, []);
   useEffect(() => {
     if (typeof window === "undefined") {
       console.log("miss");
@@ -16,6 +33,7 @@ export default function AMapContainer({ locations }: Prop) {
       (window as any)._AMapSecurityConfig = {
         securityJsCode: "a6475c5f871996a8a1e544251b263822",
       };
+
       import("@amap/amap-jsapi-loader").then((AMapLoader) => {
         AMapLoader.load({
           key: "559e609208e3e6d726a285abfbc116f8", //申请好的 Web 端开发者 Key，首次调用 load 时必填
@@ -52,6 +70,16 @@ export default function AMapContainer({ locations }: Prop) {
               });
               map.add(marker);
             });
+            if (Number.isNaN(lo) || Number.isNaN(la)) {
+              return true;
+            }
+            const position = new AMap.LngLat(lo + 0.006, la + 0.0001);
+            const marker = new AMap.Marker({
+              position: position,
+              icon: "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-via-marker.png",
+              offset: new AMap.Pixel(-13, -30),
+            });
+            map.add(marker);
           })
           .catch((e) => {
             console.log(e);
@@ -62,6 +90,6 @@ export default function AMapContainer({ locations }: Prop) {
     return () => {
       map?.destroy();
     };
-  }, []);
+  }, [la, lo]);
   return <div id="map-container" className=" min-h-screen -my-14"></div>;
 }
