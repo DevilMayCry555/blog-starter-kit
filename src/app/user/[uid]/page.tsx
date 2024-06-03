8fefdcb2fe155003674c77f26f11aac9921c1c25
zx0286 <1061471799@qq.com>
2024-06-03 18:05:05 +0800

    1

--- src/app/user/[uid]/page.tsx
+++ src/app/user/[uid]/page.tsx
@@ -25,8 +25,6 @@ export default async function User({ params }: Params) {
               <a href="/backdoor/room">会议</a>
               <span className=" opacity-0">gap</span>
               <a href="/backdoor/location">足迹</a>
-              <span className=" opacity-0">gap</span>
-              <a href="/map#117.0150/36.6610">flutter map</a>
             </>
           ) : null}
           <div>
