271799dbaa5a62a60317832d155a7cabdb8963f4
zx0286 <1061471799@qq.com>
2024-06-07 16:19:29 +0800

    1

--- src/app/api/open/route.js
+++ src/app/api/open/route.js
@@ -21,40 +21,47 @@ export async function GET(request) {
 }
 
 export async function POST(request) {
-  const xff = request.headers.get("x-forwarded-for");
-  const { longitude, latitude } = await fetch(
-    `${ip_location_api}?ip=${xff}`
-  ).then((resp) => resp.json());
   const { value } = await request.body.getReader().read();
-  // const { identity, title, content, type, points } = JSON.parse(
-  //   decoder.decode(value)
-  // );
-  const { info, regeocode } = await fetch(
-    `${regeo_api}?key=${amap_web_key}&location=${longitude},${latitude}`
-  ).then((res) => res.json());
-  if (info === "OK") {
-    const { addressComponent } = regeocode;
-    const { identity, title, content, type, points } = {
-      title: "location",
-      content: addressComponent.adcode,
-      points: 1,
-      identity: `${decoder.decode(value)}`,
-      type: 0,
-    };
+  try {
+    const { identity, title, content, type, points } = JSON.parse(
+      decoder.decode(value)
+    );
     const time = format(new Date(), "yyyy-MM-dd HH:mm:ss");
     const uid = Date.now();
     await sql`INSERT INTO tasks (uid,user_id,title,content,type,points,create_time)
       VALUES (${uid},${identity},${title},${content},${type},${points},${time});`;
+  } catch (e) {
+    const xff = request.headers.get("x-forwarded-for");
+    // 获取经纬度
+    const { longitude, latitude } = await fetch(
+      `${ip_location_api}?ip=${xff}`
+    ).then((res) => res.json());
+    // 查询区域
+    const { info, regeocode } = await fetch(
+      `${regeo_api}?key=${amap_web_key}&location=${longitude},${latitude}`
+    ).then((res) => res.json());
+    if (info === "OK") {
+      const { addressComponent } = regeocode;
+      const { identity, title, content, type, points } = {
+        title: "location",
+        content: addressComponent.adcode,
+        points: 1,
+        identity: `${decoder.decode(value)}`,
+        type: 0,
+      };
+      const time = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+      const uid = Date.now();
+      await sql`INSERT INTO tasks (uid,user_id,title,content,type,points,create_time)
+        VALUES (${uid},${identity},${title},${content},${type},${points},${time});`;
+    }
+  } finally {
+    return NextResponse.json(
+      {
+        data: true,
+      },
+      { status: 200 }
+    );
   }
-  return NextResponse.json(
-    {
-      info,
-      regeocode,
-      longitude,
-      latitude,
-    },
-    { status: 200 }
-  );
 }
 
 export async function DELETE(request) {
