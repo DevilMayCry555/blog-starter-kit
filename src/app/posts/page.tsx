451d8266664f1be21bf6d83967669a0f9937fb4a
zx0286 <1061471799@qq.com>
2024-06-24 16:23:53 +0800

    1

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,16 +3,8 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
-import { fetchUser } from "@/lib/sql";
-import { notFound } from "next/navigation";
 
 export default async function Index() {
-  // 未登录
-  const userinfo = await fetchUser();
-  if (!userinfo) {
-    return notFound();
-  }
-
   const allPosts = getAllPosts();
 
   const heroPost = allPosts[0];
