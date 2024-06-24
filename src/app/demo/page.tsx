cd33cb50393898ea85f42c3062c26e526a45c3d0
zx0286 <1061471799@qq.com>
2024-06-24 16:34:42 +0800

    1

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,15 +1,15 @@
-// import { BASE_URL } from "@/lib/constants";
-// import Chat from "./gpt";
+import { BASE_URL } from "@/lib/constants";
+import Chat from "./gpt";
 
-// export default async function Demo() {
-//   const { content } = await fetch(BASE_URL + "/api/stream").then((res) =>
-//     res.json()
-//   );
-//   const { remain_quota, used_quota } = content;
-//   const usage = 100 - (used_quota * 100) / (used_quota + remain_quota);
-
-//   return <Chat usage={usage} />;
-// }
 export default async function Demo() {
-  return <iframe src="https://gpt4o.so/zh-CN" className=" flex-1"></iframe>;
+  const { content } = await fetch(BASE_URL + "/api/stream").then((res) =>
+    res.json()
+  );
+  const { remain_quota, used_quota } = content;
+  const usage = 100 - (used_quota * 100) / (used_quota + remain_quota);
+
+  return <Chat usage={usage} />;
 }
+// export default async function Demo() {
+//   return <iframe src="https://gpt4o.so/zh-CN" className=" flex-1"></iframe>;
+// }
