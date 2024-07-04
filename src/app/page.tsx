de238d0ce83601cc4d021d1de170d963828f6ae3
zx0286 <1061471799@qq.com>
2024-07-04 17:12:33 +0800

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ const transfer = (obj: { [k: string]: any }) =>
 
 const Pages = transfer({
   "/posts": "他她",
-  "/map": "天气",
-  "/meeting": "贴吧",
   "/draw": "画板",
   "/guess": "竞猜",
-  "/demo": "GPT",
   "/three": "3D",
-  "/food": "卡路里",
   "/gcores": "Gcores",
 });
 
-const Android = transfer({
+const Tools = transfer({
+  "/meeting": "贴吧",
+  "/map": "天气",
+  "/demo": "GPT",
+  "/food": "卡路里",
   "/deep.apk": "Deep@2024.06",
 });
 
@@ -31,7 +31,7 @@ export default async function Index() {
     <main className=" m-auto w-4/5 flex flex-col lg:flex-row">
       {transfer({
         Pages,
-        Android,
+        Tools,
         Control,
       }).map((it, idx) => (
         <BaseCard title={it.label} key={idx}>
