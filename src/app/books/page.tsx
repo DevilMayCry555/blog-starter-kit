787db0f06595d33e95897681741e995cd07fe9db
liuy <1061471799@qq.com>
2024-07-14 19:12:13 +0800

    1

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -4,13 +4,18 @@ import { Link } from "@nextui-org/react";
 
 const books = {
   104016: "怪谈游戏设计师",
+  110089: "天才俱乐部",
+  113355: "剑出衡山",
+  99079: "谁让他修仙的！",
+  32276: "第一次魔法世界大战",
+  114434: "从婴儿开始入道",
 };
 
 export default async function Books() {
   return (
-    <main className=" flex-1 flex flex-col">
+    <main className=" m-auto flex flex-col">
       {Object.entries(books).map((it) => (
-        <Link key={it[0]} href={`/books/${it[0]}`}>
+        <Link className=" my-4" key={it[0]} href={`/books/${it[0]}`}>
           {it[1]}
         </Link>
       ))}
