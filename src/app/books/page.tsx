c5efd43460ba3b8675c540142aaaa69bbc0316cd
zx0286 <1061471799@qq.com>
2024-07-15 09:40:00 +0800

    1

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,24 +1,39 @@
 import { Link } from "@nextui-org/react";
 
-// const types = [];
-
 const books = {
   104016: "怪谈游戏设计师",
   110089: "天才俱乐部",
   113355: "剑出衡山",
   99079: "谁让他修仙的！",
-  32276: "第一次魔法世界大战",
-  114434: "从婴儿开始入道",
 };
 
 export default async function Books() {
   return (
     <main className=" m-auto flex flex-col">
-      {Object.entries(books).map((it) => (
-        <Link className=" my-4" key={it[0]} href={`/books/${it[0]}`}>
-          {it[1]}
-        </Link>
-      ))}
+      <div>武侠仙侠</div>
+      {Object.entries(books)
+        .filter((it) => ["99079", "113355"].includes(it[0]))
+        .map((it) => (
+          <Link className=" my-4" key={it[0]} href={`/books/${it[0]}`}>
+            {it[1]}
+          </Link>
+        ))}
+      <div>科幻</div>
+      {Object.entries(books)
+        .filter((it) => ["110089"].includes(it[0]))
+        .map((it) => (
+          <Link className=" my-4" key={it[0]} href={`/books/${it[0]}`}>
+            {it[1]}
+          </Link>
+        ))}
+      <div>悬疑</div>
+      {Object.entries(books)
+        .filter((it) => ["104016"].includes(it[0]))
+        .map((it) => (
+          <Link className=" my-4" key={it[0]} href={`/books/${it[0]}`}>
+            {it[1]}
+          </Link>
+        ))}
     </main>
   );
 }
