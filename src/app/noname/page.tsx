4453973079b9b2b5162ef494fe5dd5e75a216056
zx0286 <1061471799@qq.com>
2024-07-22 14:50:07 +0800

    1

--- src/app/noname/page.tsx
+++ src/app/noname/page.tsx
@@ -3,11 +3,14 @@ export default async function Noname() {
   //   console.log(parts);
   return (
     <main className=" flex-1">
-      <iframe
-        src="https://spmario233.github.io/noname/index.html"
-        width="100%"
-        height="100%"
-      ></iframe>
+      <div className=" relative" style={{ paddingTop: "56.25%" }}>
+        <iframe
+          src="https://spmario233.github.io/noname/index.html"
+          className=" absolute top-0"
+          width="100%"
+          height="100%"
+        ></iframe>
+      </div>
     </main>
   );
 }
