bad0ca83f0303b4f50da47ca996f2b72f7f9eb0c
zx0286 <1061471799@qq.com>
2024-07-23 14:46:33 +0800

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const transfer = (obj: { [k: string]: any }) =>
 
 const Pages = transfer({
   "/posts": "他她",
-  "/meeting": "异议",
+  "/meeting": "议异",
   "/draw": "烩绘",
   "/guess": "话画",
   "/eat": "贩饭",
