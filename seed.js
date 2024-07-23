9e3683b413047ab75ab34b6f5d4e872ce4b522a2
zx0286 <1061471799@qq.com>
2024-07-23 13:39:11 +0800

    1

--- seed.js
+++ seed.js
@@ -2,8 +2,6 @@ const { sql } = require("@vercel/postgres");
 // 已存在的表
 // 用户
 // CREATE TABLE users (uid VARCHAR(50) PRIMARY KEY,username VARCHAR(20),birthday VARCHAR(20),admin CHAR(1),create_time VARCHAR(20),update_time VARCHAR(20),draw VARCHAR(60000));
-// 聊天
-// CREATE TABLE chats (uid VARCHAR(50),user_id VARCHAR(50),user_name VARCHAR(20),content VARCHAR(20),create_time VARCHAR(20));
 // 房间
 // CREATE TABLE rooms (uid VARCHAR(50) PRIMARY KEY,password VARCHAR(20));
 // 画板
