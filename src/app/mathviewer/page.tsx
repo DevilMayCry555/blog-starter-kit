d4901decc7282bb0b8bdade16c6d5e29547c4834
zx0286 <1061471799@qq.com>
2024-07-29 11:22:39 +0800

    1

--- src/app/mathviewer/page.tsx
+++ src/app/mathviewer/page.tsx
@@ -1,9 +1,9 @@
-// import Viewer from "./viewer";
+import MathJaxEditor from "./viewer";
 
 export default async function Mathviewer() {
   return (
     <main className=" flex-1">
-      <iframe src="/mathviewer.html" width="100%" height="100%"></iframe>
+      <MathJaxEditor />
     </main>
   );
 }
