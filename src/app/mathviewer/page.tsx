547ec064f0d0efe01c9089eddbfc3e3208d273bd
zx0286 <1061471799@qq.com>
2024-07-29 11:16:50 +0800

    1

--- src/app/mathviewer/page.tsx
+++ src/app/mathviewer/page.tsx
@@ -3,7 +3,7 @@
 export default async function Mathviewer() {
   return (
     <main className=" flex-1">
-      <iframe src="./qqq.html"></iframe>
+      <iframe src="/mathviewer.html" width="100%" height="100%"></iframe>
     </main>
   );
 }
