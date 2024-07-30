803225c0e9169fb7dd12a1b0b1b23733d5993969
zx0286 <1061471799@qq.com>
2024-07-30 11:04:43 +0800

    1

--- src/app/mathviewer/editor.tsx
+++ src/app/mathviewer/editor.tsx
@@ -2,14 +2,148 @@
 
 import { useEffect, useMemo, useState } from "react";
 import "./style.css";
+import { Button } from "@nextui-org/react";
 
 const getuuid = () => `tyd-${Math.random()}`;
-let once = true;
 interface Part {
   id: string;
   type?: string;
   children?: Part[];
   [k: string]: any;
+}
+
+const getChildern = (type = "") => {
+  if (["equal", "add", "subtract", "multiply", "divide"].includes(type)) {
+    return [{ id: getuuid() }, { id: getuuid() }];
+  }
+  if (["square", "sqrt"].includes(type)) {
+    return [{ id: getuuid() }];
+  }
+  return [];
+};
+const onPartChange = (state: Part[], current: string, type: string): Part[] => {
+  return state.map((it) => {
+    const { id, children = [] } = it;
+    if (current === id) {
+      return {
+        ...it,
+        type,
+        children: getChildern(type),
+      };
+    }
+    if (children.length > 0) {
+      return {
+        ...it,
+        children: onPartChange(children, current, type),
+      };
+    }
+    return it;
+  });
+};
+
+export default function Editor() {
+  const [current, select] = useState("");
+  const [inputs, set] = useState<Part[]>([{ id: getuuid() }]);
+
+  // const transfer = useMemo(() => {
+  //   const r = inputs
+  //     .map((it) => {
+  //       return handle(it);
+  //     })
+  //     .join("");
+  //   return (
+  //     <div
+  //       dangerouslySetInnerHTML={{
+  //         __html: `<math xmlns="http://www.w3.org/1998/Math/MathML" display="block">${r}</math>`,
+  //       }}
+  //     ></div>
+  //   );
+  // }, [inputs]);
+
+  const onAction = (type = "") => {
+    if (type === "clear") {
+      set([{ id: getuuid() }]);
+      select("");
+      return;
+    }
+    set((state) => onPartChange(state, current, type));
+  };
+
+  useEffect(() => {
+    // const container = document.getElementById("math-editor-container");
+    // if (container) {
+    //   console.log(container);
+    //   container.addEventListener("click", (e) => {
+    //     const { id } = e.target as any;
+    //     select(id);
+    //   });
+    // }
+    window.addEventListener("message", function (e) {
+      select(e.data);
+    });
+    const f = document.getElementById(
+      "math-editor-iframe"
+    ) as HTMLIFrameElement;
+    if (!current) {
+      f.addEventListener("load", function (e) {
+        f.contentWindow?.postMessage(JSON.stringify(inputs));
+      });
+    } else {
+      f.contentWindow?.postMessage(JSON.stringify(inputs));
+      select("");
+    }
+  }, [inputs]);
+  return (
+    <>
+      <div className=" flex flex-wrap justify-center mb-12">
+        <Button className=" m-2 text-large" onClick={() => onAction("add")}>
+          {" "}
+          +{" "}
+        </Button>
+        <Button
+          className=" m-2 text-large"
+          onClick={() => onAction("subtract")}
+        >
+          {" "}
+          -{" "}
+        </Button>
+        <Button
+          className=" m-2 text-large"
+          onClick={() => onAction("multiply")}
+        >
+          {" "}
+          *{" "}
+        </Button>
+        <Button className=" m-2 text-large" onClick={() => onAction("divide")}>
+          {" "}
+          /{" "}
+        </Button>
+        <Button className=" m-2 text-large" onClick={() => onAction("equal")}>
+          {" "}
+          ={" "}
+        </Button>
+        <Button className=" m-2 text-large" onClick={() => onAction("square")}>
+          {" "}
+          x²{" "}
+        </Button>
+        <Button className=" m-2 text-large" onClick={() => onAction("sqrt")}>
+          {" "}
+          &#x221A;{" "}
+        </Button>
+        <Button className=" m-2 text-large" onClick={() => onAction("clear")}>
+          clear
+        </Button>
+      </div>
+      {/* <div id="math-editor-container">{transfer}</div> */}
+      <iframe
+        id="math-editor-iframe"
+        src="/mathml.html"
+        width="100%"
+        height={200}
+        className=" shadow-inner bg-white"
+      ></iframe>
+    </>
+  );
 }
 const handle = ({ id = "", type = "", children = [] }: Part): string => {
   const parts = children.map((it) => handle(it));
@@ -51,148 +185,3 @@ const handle = ({ id = "", type = "", children = [] }: Part): string => {
 
   return `<mi><input class="math-random" id="${id}" type="text" placeholder="..." ></mi>`;
 };
-
-const onPartChange = (state: Part[], current: string, type: string): Part[] => {
-  return state.map((it) => {
-    const { id, children = [] } = it;
-    if (current === id) {
-      const p1 = { id: getuuid() };
-      const p2 = { id: getuuid() };
-      return {
-        ...it,
-        type,
-        children: [p1, p2],
-      };
-    }
-    if (children.length > 0) {
-      return {
-        ...it,
-        children: onPartChange(children, current, type),
-      };
-    }
-    return it;
-  });
-};
-const onSelfChange = (state: Part[], current: string, type: string): Part[] => {
-  return state.map((it) => {
-    const { id, children = [] } = it;
-    if (current === id) {
-      return {
-        ...it,
-        type,
-        children: [{ id: getuuid() }],
-      };
-    }
-    if (children.length > 0) {
-      return {
-        ...it,
-        children: onSelfChange(children, current, type),
-      };
-    }
-    return it;
-  });
-};
-
-export default function Editor() {
-  const [current, select] = useState("");
-  const [inputs, set] = useState<Part[]>([{ id: getuuid() }]);
-
-  const transfer = useMemo(() => {
-    const r = inputs
-      .map((it) => {
-        return handle(it);
-      })
-      .join("");
-    return (
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `<math xmlns="http://www.w3.org/1998/Math/MathML" display="block">${r}</math>`,
-        }}
-      ></div>
-    );
-  }, [inputs]);
-
-  const onAction = (type = "") => {
-    if (!current) {
-      return;
-    }
-    if (["equal", "add", "subtract", "multiply", "divide"].includes(type)) {
-      set((state) => onPartChange(state, current, type));
-    }
-    if (["square", "sqrt"].includes(type)) {
-      set((state) => onSelfChange(state, current, type));
-    }
-    select("");
-  };
-
-  useEffect(() => {
-    if (!once) {
-      return;
-    }
-    once = false;
-    const container = document.getElementById("math-editor-container");
-    if (container) {
-      console.log(container);
-      container.addEventListener("click", (e) => {
-        const { id } = e.target as any;
-        select(id);
-      });
-    }
-  }, []);
-  return (
-    <>
-      <div className=" flex flex-wrap justify-center mb-12">
-        <button
-          className=" w-12 bg-slate-100 m-2"
-          onClick={() => onAction("add")}
-        >
-          {" "}
-          +{" "}
-        </button>
-        <button
-          className=" w-12 bg-slate-100 m-2"
-          onClick={() => onAction("subtract")}
-        >
-          {" "}
-          -{" "}
-        </button>
-        <button
-          className=" w-12 bg-slate-100 m-2"
-          onClick={() => onAction("multiply")}
-        >
-          {" "}
-          *{" "}
-        </button>
-        <button
-          className=" w-12 bg-slate-100 m-2"
-          onClick={() => onAction("divide")}
-        >
-          {" "}
-          /{" "}
-        </button>
-        <button
-          className=" w-12 bg-slate-100 m-2"
-          onClick={() => onAction("equal")}
-        >
-          {" "}
-          ={" "}
-        </button>
-        <button
-          className=" w-12 bg-slate-100 m-2"
-          onClick={() => onAction("square")}
-        >
-          {" "}
-          x²{" "}
-        </button>
-        <button
-          className=" w-12 bg-slate-100 m-2"
-          onClick={() => onAction("sqrt")}
-        >
-          {" "}
-          &#x221A;{" "}
-        </button>
-      </div>
-      <div id="math-editor-container">{transfer}</div>
-    </>
-  );
-}
