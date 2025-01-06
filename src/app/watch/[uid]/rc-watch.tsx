20ff61e5f89bbe84756592a8ec0ee2180cfda470
liuy <1061471799@qq.com>
2025-01-06 23:15:38 +0800

    1

--- src/app/watch/[uid]/rc-watch.tsx
+++ src/app/watch/[uid]/rc-watch.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as Ably from "ably";
 import {
   AblyProvider,
@@ -10,6 +10,7 @@ import {
 } from "ably/react";
 import ReactPlayer from "react-player";
 import "./rc-watch.css";
+import { Button } from "@nextui-org/react";
 
 // Connect to Ably using the AblyProvider component and your API key
 const client = new Ably.Realtime({
@@ -30,35 +31,36 @@ export default function RtWatch(props: any) {
 
 function AblyPubChat({ hardcore }: any) {
   const [messages, setMessages] = useState<Ably.Message[]>([]);
-  const [talk, set_talk] = useState("");
+  const [ss, set_ss] = useState(0);
   const [url, set_url] = useState("");
+  const playerRef = useRef(null as any);
 
   // Create a channel called 'get-started' and subscribe to all messages with the name 'first' using the useChannel hook
   const { channel } = useChannel(name, hardcore, (message) => {
-    setMessages((previousMessages) => [...previousMessages, message]);
+    // setMessages((previousMessages) => [...previousMessages, message]);
+    const { time } = JSON.parse(message.data);
+    playerRef.current?.seekTo(time);
   });
 
   useConnectionStateListener("connected", () => {
     console.log("Connected success!");
   });
 
-  const onSubmit = () => {
-    channel.publish(
-      hardcore,
-      JSON.stringify({
-        content: talk,
-      })
-    );
-    set_talk("");
-  };
   useEffect(() => {
     set_url(location.hash.replace(/^#/, ""));
   });
-  const onPlay = (...arg: any[]) => {
-    console.log(...arg);
-  };
-  const onPause = (...arg: any[]) => {
-    console.log(...arg);
+  const onSeek = (time: number) => {
+    if (`${ss}` === `${time}`) {
+      return;
+    }
+    console.log("lalala", ss, time);
+    set_ss(time);
+    channel.publish(
+      hardcore,
+      JSON.stringify({
+        time,
+      })
+    );
   };
   if (!url) {
     return "lalala";
@@ -67,13 +69,12 @@ function AblyPubChat({ hardcore }: any) {
     <div>
       <div className="base-radio-player-wrap">
         <ReactPlayer
+          ref={playerRef}
           url={url}
           controls
           width={"100%"}
           height={"100%"}
-          onPlay={onPlay}
-          onProgress={onPlay}
-          onPause={onPause}
+          onSeek={onSeek}
         />
       </div>
     </div>
